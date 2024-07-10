00e3184b7c8fad9f87744dafb9ce1b51e1d8184c
MINT <mint.2978@gmail.com>
2024-07-10 14:11:50 +0700

    feat(algorithm): solve ploblem

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,12 @@
 function twoSum(numbers, target) {
-	// เริ่มเขียนโค้ดตรงนี้จ้า
-};
+  // เริ่มเขียนโค้ดตรงนี้จ้า
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = i; j < numbers.length; j++) {
+      if (numbers[i] + numbers[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+}
+
+console.log(twoSum([2, 7, 11, 15], 9));
